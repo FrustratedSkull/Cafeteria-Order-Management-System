0086089940313a57dd52e5bd0e9e1f06bc17200b
Nikhil Mishra <36605783+FrustratedSkull@users.noreply.github.com>
2019-08-21 09:28:22 +0530

    2.1

--- Cafeteria Order Management System/src/Cafeteria Order Management System.c
+++ Cafeteria Order Management System/src/Cafeteria Order Management System.c
@@ -11,7 +11,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define SIZE 40
+#define ROW 40
+#define COL 30
 // Structure Declaration
 typedef struct Dataset{
 	char category[40];
@@ -19,22 +20,33 @@ typedef struct Dataset{
 	int  price;
 	struct Dataset *next;
 }Dataset;
-
+// Order Details
+typedef struct Order{
+	char category[40];
+	char item[20];
+	int price;
+	int orderNumber;
+	struct Order *next;
+}Order;
 // Global Variables
 	FILE *file;
 	char dataset[1000];
 	Dataset *Head = NULL;
+	Order *orderHead = NULL;
+	char userChoice[ROW][COL];
 // Function Declaration
 void fileOpen();
 void initialization();
-void createNode(char[SIZE][SIZE]);
+void createNode(char[ROW][COL]);
 void showNode();
+void takeOrder();
 void fileClose();
 int main(void) {
 	fileOpen();
 	if(file != NULL){
 		initialization();
 		showNode();
+		takeOrder();
 	}
 	else {
 		printf("\n\t !! Warning !! \n\t Data Source file data.txt is missing");
@@ -55,8 +67,10 @@ void fileOpen(){
  *Data is captured from data.txt source file
 */
 void initialization(){
-	char category[SIZE][SIZE] = {};
+	char category[ROW][COL] = {};
 	int j = 0;
+	orderHead = (Order*)malloc(sizeof(Order));
+	orderHead->next = NULL;
 	Head = (Dataset*)malloc(sizeof(Dataset));
 	Head->next = NULL;
 	while(fgets(dataset, 50, file) != NULL){
@@ -66,7 +80,7 @@ void initialization(){
 	createNode(category);
 
 }
-void createNode(char data[SIZE][SIZE]){
+void createNode(char data[ROW][COL]){
 	Dataset *temp = (Dataset*)malloc(sizeof(Dataset));
 	int i = 0;
 	while(i < 32){
@@ -127,6 +141,36 @@ void showNode(){
 	}
 	fflush(stdin);
 	getchar();
+}
+void takeOrder(){
+	Dataset *temp = (Dataset*)malloc(sizeof(Dataset));
+	Order *order = (Order*)malloc(sizeof(Order));
+	int i = 0;
+	char choose;
+	char tempItem[40];
+	while(choose != 'N'){
+		printf("\n\t What would you like to Order: ");
+		gets(userChoice[i]);
+		printf("\n\t Quantity: ");
+		gets(userChoice[++i]);
+		printf("\n\t Do you want to Order more? (Y/N): ");
+		fflush(stdin);
+		choose = getchar();
+	}
+	i = 0;
+	temp = Head->next;
+	if(temp != NULL){
+		strcpy(tempItem, temp->item[0]);
+		if( strcmp(userChoice[i], tempItem) == 0){
+			strcpy(order->category	,	temp->category	);
+			strcpy(order->item		, 	temp->item		);
+			order->price = temp->price;
+		}
+		else{
+
+		}
+		temp = temp->next;
+	}
 }
 void fileClose(){
 	fclose(file);
