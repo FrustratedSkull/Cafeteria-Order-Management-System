825458d0e99713b0c45b24397d5a06bfe747030e
Nikhil Mishra <36605783+FrustratedSkull@users.noreply.github.com>
2019-08-30 23:16:57 +0530

    2.4

--- Cafeteria Order Management System/src/Cafeteria Order Management System.c
+++ Cafeteria Order Management System/src/Cafeteria Order Management System.c
@@ -3,8 +3,7 @@
  Name        : Cafeteria.c
  Author      : Ranjana
  Version     :
- Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
+ Description : C programming Assigment (WASE)
  ============================================================================
  */
 
@@ -14,12 +13,18 @@
 #define ROW 40
 #define COL 30
 // Structure Declaration
+/*
+ * Dataset will hold all the item details for the cafe
+ */
 typedef struct Dataset{
 	char category[40];
 	char item[20];
 	int  price;
 	struct Dataset *next;
 }Dataset;
+/*
+ * Order will hold the details for the ordered items for customer
+ */
 // Order Details
 typedef struct Order{
 	char category[40];
@@ -32,17 +37,15 @@ typedef struct Order{
 // Global Variables
 	FILE *file;
 	char dataset[1000];
-	Dataset *Head = NULL;
-	Order *orderHead = NULL;
-	char userChoice[ROW][COL];
+	Dataset *Head = NULL;			// Root node for Data set
+	Order *orderHead = NULL;		// Root node for Order
 // Function Declaration
-void fileOpen();
-void initialization();
-void createNode(char[ROW][COL]);
-void showNode();
-void takeOrder();
-void orderSuccessful();
-void fileClose();
+void fileOpen();					// To open the data source file i.e. data.txt
+void initialization();				// To initialize all the global node and variables
+void createNode(char[ROW][COL]);	// To store the data into structure after reading from file
+void showNode();					// Display the data as a Menu
+void takeOrder();					// To take user input to book the order
+void fileClose();					// To close the open file
 int main(void) {
 	fileOpen();
 	if(file != NULL){
@@ -68,21 +71,21 @@ void fileOpen(){
  *It will fill the data set variable.
  *Data is captured from data.txt source file
 */
-void initialization(){
-	char category[ROW][COL] = {};
+void initialization(){			// To initialize all the global node and variables
+	char data[ROW][COL] = {};
 	int j = 0;
 	orderHead = (Order*)malloc(sizeof(Order));
 	orderHead->next = NULL;
 	Head = (Dataset*)malloc(sizeof(Dataset));
 	Head->next = NULL;
 	while(fgets(dataset, 50, file) != NULL){
-		strcpy(category[j],dataset);
+		strcpy(data[j],dataset);
 		j++;
 	}
-	createNode(category);
+	createNode(data);
 
 }
-void createNode(char data[ROW][COL]){
+void createNode(char data[ROW][COL]){		// To store the data into structure after reading from file
 	Dataset *temp = (Dataset*)malloc(sizeof(Dataset));
 	int i = 0;
 	while(i < 32){
@@ -115,7 +118,7 @@ void createNode(char data[ROW][COL]){
 	}
 
 }
-void showNode(){
+void showNode(){				// Display the data as a Menu
 	system("cls");
 	Dataset *temp = (Dataset*)malloc(sizeof(Dataset));
 	temp = Head->next;
@@ -142,99 +145,88 @@ void showNode(){
 		temp = temp->next;
 	}
 }
-void takeOrder(){
+void takeOrder(){			// To take user input to book the order
 	Dataset *temp = (Dataset*)malloc(sizeof(Dataset));
 	Order *orderTemp = (Order*)malloc(sizeof(Order));
-	int i = 0;
-	int randNum, currentOrder;
-	randNum = rand();
+	int i = 0, quantity = 0, randNumber, totalPrice = 0;
 	char choose = 'Y';
-	while(choose != 'N'){			// Only storing item and quantity, later on price will be calculated
-		Order *order = (Order*)malloc(sizeof(Order));
-		printf("\n\t What would you like to Order: ");
+	char itemName[20];
+	randNumber = rand();
+	while(choose != 'N'){
+		// Start of taking user input
 		fflush(stdin);
-		fgets(order->item, 20,stdin);		// Item
+		printf("\n\n\t What would you like to Order: ");
+		fflush(stdin);
+		fgets(itemName, 20,stdin);		// Item
 		printf("\n\t Quantity: ");
-		scanf("%d",&order->quantity);		// Quantity
+		scanf("%d",&quantity);		// Quantity
+		// Taking copy of head nodes for both struture
+		orderTemp = orderHead;
+		temp = Head->next;
+		while(temp != NULL){
+			// Validating user input by comparing item name
+			if(strcmp(itemName, temp->item) == 0){
+				Order *newOrder = (Order*)malloc(sizeof(Order));
+				// After condition match, copying item details to order details
+				strcpy(newOrder->category, temp->category);
+				strcpy(newOrder->item, temp->item);
+				newOrder->price = 0;
+				for(i = 0; i < quantity; i++){		// Adding price according to quantity
+					newOrder->price = newOrder->price + temp->price;
+				}
+				newOrder->quantity = quantity;
+				newOrder->orderNumber = 40000 + randNumber;
+				newOrder->next = NULL;
+				orderTemp = orderHead->next;
+				// Below condition is to put the current order to the last node
+				if(orderHead->next == NULL){	// This condition will work if there is no first order
+					orderHead->next = newOrder;
+				}
+				else{
+					while(orderTemp != NULL){
+						if(orderTemp->next == NULL){
+							break;
+						}
+						orderTemp = orderTemp->next;
+					}
+					orderTemp->next = newOrder;
+				}
+			}
+			temp = temp->next;
+
+		}
+		// Asking whether user want to order more 'Y' for Yes 'N' for No
 		printf("\n\t Do you want to Order more? (Y/N): ");
 		fflush(stdin);
 		scanf("%c",&choose);
-		if(orderHead->next == NULL){
-			orderHead->next = order;
-			order->next = NULL;
-		}
-		else{
-			orderTemp = orderHead->next;
-			while(orderTemp != NULL){
-				if(orderTemp->next == NULL) break;
-				orderTemp = orderTemp->next;
-			}
-			orderTemp->next = order;
-			order->next = NULL;
-		}
-
-	}											// End of taking input
-
-	i = 0;
-	temp = Head->next;							// Price will be calculated
-
-	orderTemp = orderHead->next;
-	while(orderTemp != NULL){
-		while(temp != NULL){
-			//printf("\n %s and %s",orderTemp->item, temp->item);
-			i = 0;
-			while(orderTemp->item[i] != '\n'){
-				if(orderTemp->item[i] == temp->item[i]){
-					//printf("\n Match found");
-					strcpy(orderTemp->category, temp->category);	// Setting the category
-					orderTemp->price = 0;			// Initializing price to 0
-					for(i = 0; i < orderTemp->quantity; i++){
-						orderTemp->price = orderTemp->price + temp->price;
-						printf("\n %d",temp->price);
-					}
-					orderTemp->orderNumber = 400000 + randNum;
-					currentOrder = orderTemp->orderNumber;
-					printf("\n Success");
-
-				}
-				if(orderTemp->item[i] == '\n') break;
-				i++;
-
-			//if(strcmp(orderTemp->item, temp->item) == 0){
-
-
-			}
-			temp = temp->next;
-		}
-		orderTemp = orderTemp->next;
 	}
 
-fflush(stdin);
-getchar();
-/*								Prompt Order details
+/*								Output Order details
  *
 */
 	system("cls");
-	printf("\n\t Details for Order: %d",currentOrder);
 	orderTemp = orderHead->next;
 	if(orderTemp == NULL){
 		printf("\n\t Sorry, Order can't be booked");
 	}
 	else{
-		printf("\n\n\t Category \t Item \t Quantity \t Total Price\n");
+		printf("\n\t Details for Order: %d", orderTemp->orderNumber);
+		printf("\n\n\n\t Category \t Item \t\t\t Quantity \t\t Sub-Total\n");
+		printf("\t________________________________________________________________________");
 		while(orderTemp != NULL){
-			printf("\n\t %s \t\t\t %s \t\t\t\t %d \t\t\t %d ",orderTemp->category, orderTemp->item,
+			totalPrice = totalPrice + orderTemp->price;
+			printf("\n\n\t %s \t\t\t %s \t\t\t\t\t\t %d \t\t\t %d ",orderTemp->category, orderTemp->item,
 													orderTemp->quantity, orderTemp->price);
 			orderTemp = orderTemp->next;
 		}
 	}
+	printf("\n\n\n\t\t\t\t\t\t Total :\t\t %d", totalPrice);
 	fflush(stdin);
+	printf("\n\n\n\n\t Press enter to go back to main menu");
 	getchar();
+	main();
 }
 
-void orderSuccessful(){
-
-}
-void fileClose(){
+void fileClose(){		// To close the open file
 	fclose(file);
 }
